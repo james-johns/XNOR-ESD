b169370f8ad9265b8895d99339801dbabf714076
JOhn <john2.lazo@live.weu.ac.uk>
2015-04-06 21:03:47 +0100

    New files added for KIOSK

---
+++ server/main.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+
+
+<style>
+body {
+    background-color: #000000;
+} 
+#main {
+    background-color:#5C320D;
+    color:white;
+    text-align:center;
+    padding:5px;
+	width: 800px;
+    margin: 0 auto;
+
+
+}
+
+table {
+    border: 0px;
+	width: 100%;
+
+}
+td {
+    padding: 5px;
+}
+
+.bigbutton { 
+	width: 200px; 
+	height: 100px;
+} 
+
+</style>
+</head>
+<body>
+
+<div id="main">
+<table width="200" border="0">
+  <tr>
+    <td colspan="3" align="center"><h1>KIOSK MENU</h1></td>
+    </tr>
+  <tr>
+    <td align="center"><input type="submit" class="bigbutton" value="Register User" title="Subscribe" onclick="window.location.href='registeruser.php'" /></td>
+    <td align="center"><input type="submit" class="bigbutton" value="Search User" title="Subscribe" onclick="window.location.href='registeruser.php'" /> </td>
+    <td align="center"><input type="submit" class="bigbutton" value="Delete User" title="Subscribe" onclick="window.location.href='registeruser.php'" /></td>
+    </tr>
+  <tr>
+    <td align="center"><input type="submit" class="bigbutton" value="Complete Payment" title="Subscribe" onclick="window.location.href='payment.php'" /></td>
+    <td align="center"><input type="submit" class="bigbutton" value="OPEN" title="Subscribe" onclick="window.location.href='registeruser.php'" /></td>
+    <td align="center"><input type="submit" class="bigbutton" value="Logout RPU" title="Subscribe" onclick="window.location.href='registeruser.php'" /></td>
+    </tr>
+  <tr>
+    <td align="center"><input type="submit" class="bigbutton" value="Manage Track" title="Subscribe" onclick="window.location.href='registeruser.php'" /></td>
+    <td align="center"><input type="submit" class="bigbutton" value="Delete Track" title="Subscribe" onclick="window.location.href='registeruser.php'" /></td>
+    <td align="center"><input type="submit" class="bigbutton" value="Replace Track" title="Subscribe" onclick="window.location.href='registeruser.php'" /></td>
+    </tr>
+</table>
+
+</div>
+
+
+</body>
+</html>
