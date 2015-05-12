59d1dd9bd83d412e00f5667ee6404d215b6f296e
rking1101 <rking1101@gmail.com>
2015-05-13 00:09:30 +0100

    LCDDisplay.cpp fully updated to Linux coding standard,implemented all tokenising updates to remaining if statements  and removed irrelevant debug lines

--- rpu/display/LCDDisplay.cpp
+++ rpu/display/LCDDisplay.cpp
@@ -56,17 +56,17 @@ void LCDDisplay::refresh()
  
   
 	char trackOutputString [4][16];
-	char menuOutputString [4][16];
-	char errorOutputString [4][32];
+	char menuOutputString [4][17];
+	char errorOutputString [4][33];
 	int tokenNumber;
-	int j=0;
+	
 
 	for (int i=0;i>4;i++) {
 		trackOutputString[i][0]=0;
 		menuOutputString[i][0]=0;
 		errorOutputString[i][0]=0;
 	}
- 
+
 
 
 		if (playbackDirty == true & playbackString != NULL)
@@ -77,41 +77,26 @@ void LCDDisplay::refresh()
 		write(displayDevice,playbackString,1);
 		playbackDirty = false;
       	}
-
   		if (trackInfoDirty == true & trackInfoString != NULL){
-		j=0;
 		tokenNumber=0;
 		std::cout << "writing track Info String\n";
 		std::cout <<"size of the trackinfo string: " << strlen(trackInfoString) << "\n";
 		int tokenSize = strlen(trackInfoString);
-		std::cout <<"tokenSize: " << tokenSize << "\n";
-	
+      
 		for (int i = 0; i < tokenSize; i += 15) {
-		std::cout << "current i value: " << i <<"\n";
 		int tokenLength = (((tokenSize - i) >= 15) ? 15 : (tokenSize - i));
 		strncpy(trackOutputString[i/15], trackInfoString + i, tokenLength);
 		trackOutputString[i/15][tokenLength] = '\0';
 		std::cout << "tokenNumber: " << tokenNumber << "\n";
 		tokenNumber++;
 	}
-		tokenNumber--;
-
-		if(strlen(trackOutputString[tokenNumber]) <15){
-
-		for (int l=strlen(trackOutputString[tokenNumber]);l<15;l++){
-		trackOutputString[tokenNumber][l] = ' ';
+		if(strlen(trackOutputString[tokenNumber-1]) <15){
+		for (int l=strlen(trackOutputString[tokenNumber-1]);l<15;l++){
+		trackOutputString[tokenNumber-1][l] = ' ';
 	}
 	}
-		
-	      
-     
- 
-      
-
-		for(int k=0;k<=tokenNumber;k++){
-		
+		for(int k=0;k<tokenNumber;k++){
 		if (trackOutputString[k][0] != 0){
-
 		std:: cout << " string contents: " << trackOutputString[k] <<"\n";
 		write(displayDevice,displayOptionMode,sizeof(displayOptionMode));
 		write(displayDevice,blockNumber2,sizeof(blockNumber2));
@@ -122,46 +107,27 @@ void LCDDisplay::refresh()
 		trackInfoDirty = false;  
 	}
 	
-	
 		if (menuDirty == true & menuString != NULL){
 		tokenNumber=0;
-		j=0;
-		std::cout << "writing menu String\n";
-		std::cout <<"size of the menu string: " << strlen(menuString) << "\n";
-		for (int i=0;i <= strlen(menuString);i++){
-		
-		//std::cout << "current char number: " << j <<"\n";
-		// std::cout << "current char of menu String : " << menuString[i] <<"\n";
-		menuOutputString[tokenNumber][i] =  menuString[i];
-		//std::cout << "copied char to menuOutput String : " << menuOutputString[tokenNumber][i] <<"\n";
-	 
-		// std::cout << "current token Number: " << tokenNumber << "\n";
-		if (j==15)
-		{
-		std::cout << "new string created\n";
-		//std::cout << "String Created : " << menuOutputString[tokenNumber] << "\n";
+		std::cout << "writing track Info String\n";
+		std::cout <<"size of the trackinfo string: " << strlen(menuString) << "\n";
+		int tokenSize = strlen(menuString);
+      
+		for (int i = 0; i < tokenSize; i += 16) {
+		int tokenLength = (((tokenSize - i) >= 16) ? 16 : (tokenSize - i));
+		strncpy(menuOutputString[i/16], menuString + i, tokenLength);
+		menuOutputString[i/16][tokenLength] = '\0';
+		std::cout << "tokenNumber: " << tokenNumber << "\n";
 		tokenNumber++;
-	     
-		j=0;
 	}
-		j++;
-	}
-     
-		if(strlen(menuOutputString[tokenNumber]) <15)
-		{
-		for (int l=strlen(menuOutputString[tokenNumber]);l<15;l++)
-		{
-		menuOutputString[tokenNumber][l] = ' ';
+		if(strlen(menuOutputString[tokenNumber-1]) <16){
+		for (int l=strlen(menuOutputString[tokenNumber-1]);l<16;l++){
+		menuOutputString[tokenNumber-1][l] = ' ';
 	}
 	}
-
-		std::cout << "number of Strings = " << tokenNumber << "\n";
-	  
-		for(int k=0;k<=tokenNumber+1;k++)
-		{
-		if (menuOutputString[k] != NULL)
-		{
-		std::cout << "current string : " << menuOutputString[k] <<"\n";
+		for(int k=0;k<tokenNumber;k++){
+		if (menuOutputString[k][0] != 0){
+		std:: cout << " string contents: " << menuOutputString[k] <<"\n";
 		write(displayDevice,displayOptionMode,sizeof(displayOptionMode));
 		write(displayDevice,blockNumber3,sizeof(blockNumber3));
 		write(displayDevice,menuOutputString[k],strlen(menuOutputString[k]));
@@ -171,57 +137,36 @@ void LCDDisplay::refresh()
 		menuDirty = false;  
 	}
 
+	
+
 		if (errorString != NULL)
 		{
 		tokenNumber=0;
-		j=0;
+		std::cout << "writing track Info String\n";
+		std::cout <<"size of the trackinfo string: " << strlen(errorString) << "\n";
+		int tokenSize = strlen(errorString);
       
-		//display will enter option mode and wait for the next sspecial character.
-		write(displayDevice,displayOptionMode,sizeof(displayOptionMode));
-		//special command to clear the display screen
-		write(displayDevice,displayClear,sizeof(displayClear));
-
-		std::cout << "writing Error Message\n";
-		std::cout <<"size of Error Message: " << strlen(errorString) << "\n";
-		for (int i=0;i <= strlen(errorString);i++)
-		{
-		// std::cout << "current char number: " << j <<"\n";
-		//std::cout << "current char : " << menuString[i] <<"\n";
-		errorOutputString[tokenNumber][i] =  errorString[i];
-		// std::cout << "copied char : " << menuOutputString[tokenNumber][i] <<"\n";
-		j++;
-		/*if (j==15)
-		  {
-		  std::cout << "new string created\n";
-		  tokenNumber++;
-		  j=0;
-		  }*/
+		for (int i = 0; i < tokenSize; i += 32) {
+		int tokenLength = (((tokenSize - i) >= 32) ? 32 : (tokenSize - i));
+		strncpy(errorOutputString[i/32], errorString + i, tokenLength);
+		errorOutputString[i/32][tokenLength] = '\0';
+		std::cout << "tokenNumber: " << tokenNumber << "\n";
+		tokenNumber++;
 	}
-     
-
-		if(strlen(errorOutputString[tokenNumber]) <15)
-		{
-		for (int l=strlen(errorOutputString[tokenNumber]);l<15;l++)
-		{
-		errorOutputString[tokenNumber][l] = ' ';
+		if(strlen(errorOutputString[tokenNumber-1]) <32){
+		for (int l=strlen(errorOutputString[tokenNumber-1]);l<32;l++){
+		errorOutputString[tokenNumber-1][l] = ' ';
 	}
-	} 
-
-		std::cout << "number of Strings = " << tokenNumber+1 << "\n";
-		for(int k=0;k<=tokenNumber+1;k++)
-		{
-		if (errorOutputString[k] != NULL)
-		{
-		std::cout << "current string : " << errorOutputString[k] <<"\n";
+	}
+		for(int k=0;k<tokenNumber;k++){
+		if (errorOutputString[k][0] != 0){
+		std:: cout << " string contents: " << errorOutputString[k] <<"\n";
 		write(displayDevice,displayOptionMode,sizeof(displayOptionMode));
 		write(displayDevice,blockNumber1,sizeof(blockNumber1));
 		write(displayDevice,errorOutputString[k],strlen(errorOutputString[k]));
-		sleep(10);
+		sleep(5);
 	}
 	}
-		errorString = NULL;  
-
-
-
+	      	errorString = NULL;  
 	}
 	}
