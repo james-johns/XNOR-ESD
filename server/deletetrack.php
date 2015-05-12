74f22343928ca7670f5c7556c9b027ea0915a507
JOhn <john2.lazo@live.uwe.ac.uk>
2015-05-12 03:20:38 +0100

    ver 2 of addtrack.php (addtrack1) added, deletetrack and logoutrpu pages added

---
+++ server/deletetrack.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Add User</title>
+
+  <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+</head>
+<body>
+
+<?php
+
+//print_r($_POST);
+
+//if (isset($_POST['submit'])){
+	//$name = htmlentities($_POST['name'], ENT_QUOTES, 'UTF-8');	
+	//$fullname = implode('+', $_POST['fullname']);
+	
+
+//}
+?>
+
+
+
+<form id="deletetrackform" action="server.php">
+<fieldset>
+<legend>Delete Track:</legend>
+<table width="200" border="1">
+  <tr>
+    <td><input type="text" size="32" name="findtrack" placeholder="Track Name" required></td>
+  </tr>
+</table>
+
+
+</fieldset>
+
+    <br>
+    <center>
+      <p>
+       	<input type="submit" value="Delete Track">
+        <input type="reset" value="Clear Field">
+        <input type="button" onclick="window.location.replace('main.php')" value="Cancel" />
+</form>
+<div id="result"></div>
+
+<script type="text/javascript">
+$("#deletetrackform").submit(function(event) {
+
+  /* stop submitting form */
+  event.preventDefault();
+
+  /* get some values from elements on the page: */
+  var $form = $( this ),
+  	act = "6",
+    tn = $form.find( "input[name='findtrack']" ).val(),
+    url = $form.attr( "action" );
+
+
+  /* Send the data using post */
+  var posting = $.post( url, {	action: act,
+  								findtrack: tn } );
+
+  /* Put the results in a div */
+  posting.done(function( data ) {
+	  switch(data)//needs to parse actual response data
+	  {
+		case 'OK': 
+			alert('Track Deleted');
+			break;
+		case 'FALSE':
+			alert('Track Not Found');
+			break;
+		  
+	  }
+	  
+	var content = $( data ).find( "#content" );
+	$( "#result" ).empty().append( content );
+
+  });
+});
+
+</script>
+</body>
+</html>
