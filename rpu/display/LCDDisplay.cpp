7effb16b914dc924b68bedb29a94117cf77ebb44
rking1101 <rking1101@gmail.com>
2015-05-17 03:10:59 +0100

    Updated LCDDisplay.cpp to fix rogue character errors during display,fixed errors created during refactoring and added playback character translation. Also updated display test case to be more comprehensive

--- rpu/display/LCDDisplay.cpp
+++ rpu/display/LCDDisplay.cpp
@@ -1,7 +1,7 @@
 
 
 #include <stdlib.h>
-
+#include <stdexcept>
 #include <LCDDisplay.h>
 
 
@@ -15,29 +15,34 @@ LCDDisplay::LCDDisplay() : Display()
 		displayDevice = open("/dev/ttyUSB1", O_RDWR) ;
 		if( displayDevice == -1) {
 			printf( "Cannot open USB device on /dev/ttyusb1\n\n");
-			return;
+				
+			throw std::invalid_argument( "Display Device not detected" );
 		}
 	}
 	std::cout << "file opened\n";
 
 	displayOptionMode = (char) 254;
 	displayClear = (char) 1;
+	
+	
 
 	tcgetattr(displayDevice, &options);
 	cfsetispeed(&options, B9600);
 	cfsetospeed(&options, B9600);
 	tcsetattr(displayDevice, TCSANOW, &options);
 
-	//display will enter option mode and wait for the next sspecial character.
-	write(displayDevice, &displayOptionMode, 1);
-	//special command to clear the display screen
-	write(displayDevice, &displayClear, 1);
 
 	blocks = (struct displayBlock *) malloc(sizeof(struct displayBlock)*4);
 	blocks[0].start = 0;	blocks[0].length = 1;
 	blocks[1].start = 1;	blocks[1].length = 15;
 	blocks[2].start = 16;	blocks[2].length = 16;
 	blocks[3].start = 0;	blocks[3].length = 32;
+
+	//display will enter option mode and wait for the next special character.
+	write(displayDevice, &displayOptionMode, sizeof(displayOptionMode));
+	//special command to clear the display screen
+	write(displayDevice, &displayClear, sizeof(displayClear));
+	std::cout << "clearing display" << std::endl;
 }
 
 LCDDisplay::~LCDDisplay()
@@ -51,37 +56,68 @@ void LCDDisplay::refresh()
 	char blockNumber1[1]={128};//first character of the display
 	char blockNumber2[1]={129};//2nd character of the display
 	char blockNumber3[1]={192};//10th character of the display
-
+	char displayOptionMode[1] = {254};
+	char displayClear[1] = {1};
 	char trackOutputString [4][16];
 	char menuOutputString [4][17];
-	char errorOutputString [4][33];
+	char errorOutputString [1][33];
+	char playbackOutputString[1];
 	int tokenNumber;
 
+	char playIcon[1]={62};
+	char pauseIcon[1]={219};
+	char rewindIcon[1]={127};
+	char fForwardIcon[1]={126};
+
 	for (int i=0;i>4;i++) {
+		playbackOutputString[0]=0;
 		trackOutputString[i][0]=0;
 		menuOutputString[i][0]=0;
 		errorOutputString[i][0]=0;
 	}
 
 	if (playbackDirty == true & playbackString != NULL) {
-		std::cout << "writing playback String\n";
-		write(displayDevice, &displayOptionMode, 1);
+		tokenNumber=0;
+		std::cout << "playback Button Pressed\n";
+		int tokenSize = strlen(playbackString);
+      		
+		switch(*playbackString){
+		case 'p':
+		
+			strncpy(playbackOutputString,playIcon,8);
+			std::cout << "play symbol" <<  std::endl;
+			break;
+		case 'w':
+			strncpy(playbackOutputString,pauseIcon,8);
+			std::cout << "Rewind symbol" <<  std::endl;
+			break;
+		case 'f':
+			strncpy(playbackOutputString,fForwardIcon,8);
+			std::cout << "Fastforward symbol" <<  std::endl;
+			break;
+		case 'r':
+			strncpy(playbackOutputString,rewindIcon,8);
+			std::cout << "Pause symbol" << std::endl;
+			break;
+		}
+
+		write(displayDevice, displayOptionMode, 1);
 		write(displayDevice, blockNumber1, sizeof(blockNumber1));
-		write(displayDevice, playbackString, 1);
+		write(displayDevice, playbackOutputString, 1);
 		playbackDirty = false;
-      	}
+	}
 
 	if (trackInfoDirty == true & trackInfoString != NULL) {
 		tokenNumber=0;
-		std::cout << "writing track Info String\n";
-		std::cout <<"size of the trackinfo string: " << strlen(trackInfoString) << "\n";
+		std::cout << "Playing Track\n";
 		int tokenSize = strlen(trackInfoString);
-      
+      		std::cout <<"size of the trackinfo string: " << tokenSize << std::endl;
+
 		for (int i = 0; i < tokenSize; i += 15) {
 			int tokenLength = (((tokenSize - i) >= 15) ? 15 : (tokenSize - i));
 			strncpy(trackOutputString[i/15], trackInfoString + i, tokenLength);
 			trackOutputString[i/15][tokenLength] = '\0';
-			std::cout << "tokenNumber: " << tokenNumber << "\n";
+			std::cout << "tokenNumber: " << tokenNumber << std::endl;
 			tokenNumber++;
 		}
 
@@ -93,10 +129,10 @@ void LCDDisplay::refresh()
 
 		for(int k=0;k<tokenNumber;k++) {
 			if (trackOutputString[k][0] != 0) {
-				std:: cout << " string contents: " << trackOutputString[k] <<"\n";
-				write(displayDevice, &displayOptionMode, 1);
+				std:: cout << " string contents: " << trackOutputString[k] <<std::endl;
+				write(displayDevice, displayOptionMode, 1);
 				write(displayDevice, blockNumber2, sizeof(blockNumber2));
-				write(displayDevice, trackOutputString[k], strlen(trackOutputString[k]));
+				write(displayDevice, trackOutputString[k], 15);
 				sleep(5);
 			}
 		}
@@ -105,30 +141,30 @@ void LCDDisplay::refresh()
 
 	if (menuDirty == true & menuString != NULL) {
 		tokenNumber=0;
-		std::cout << "writing track Info String\n";
-		std::cout <<"size of the trackinfo string: " << strlen(menuString) << "\n";
+		std::cout << "Scroll button detected\n";
 		int tokenSize = strlen(menuString);
+		std::cout <<"size of the menu string: " << tokenSize << std::endl;
 
 		for (int i = 0; i < tokenSize; i += 16) {
 			int tokenLength = (((tokenSize - i) >= 16) ? 16 : (tokenSize - i));
 			strncpy(menuOutputString[i/16], menuString + i, tokenLength);
 			menuOutputString[i/16][tokenLength] = '\0';
-			std::cout << "tokenNumber: " << tokenNumber << "\n";
+			std::cout << "tokenNumber: " << tokenNumber << std::endl;
 			tokenNumber++;
 		}
 
 		if(strlen(menuOutputString[tokenNumber-1]) <16) {
-			for (int l=strlen(menuOutputString[tokenNumber-1]);l<16;l++) {
+			for (int l=strlen(menuOutputString[tokenNumber-1]);l<=16;l++) {
 				menuOutputString[tokenNumber-1][l] = ' ';
 			}
 		}
 
 		for(int k=0;k<tokenNumber;k++) {
 			if (menuOutputString[k][0] != 0) {
-				std:: cout << " string contents: " << menuOutputString[k] <<"\n";
-				write(displayDevice, &displayOptionMode, 1);
+				std:: cout << " string contents: " << menuOutputString[k] <<std::endl;
+				write(displayDevice, displayOptionMode, sizeof(displayOptionMode));
 				write(displayDevice, blockNumber3, sizeof(blockNumber3));
-				write(displayDevice, menuOutputString[k], strlen(menuOutputString[k]));
+				write(displayDevice, menuOutputString[k], 16);
 				sleep(5);
 			}
 		}
@@ -137,15 +173,19 @@ void LCDDisplay::refresh()
 
 	if (errorString != NULL) {
 		tokenNumber=0;
-		std::cout << "writing track Info String\n";
-		std::cout <<"size of the trackinfo string: " << strlen(errorString) << "\n";
+		std::cout << "ERROR in Progress\n";
 		int tokenSize = strlen(errorString);
-
+		std::cout <<"size of the Error string: " << tokenSize << std::endl;
+		//display will enter option mode and wait for the next special character.
+		write(displayDevice, &displayOptionMode, 1);
+		//special command to clear the display screen
+		write(displayDevice, &displayClear, 1);
+	
 		for (int i = 0; i < tokenSize; i += 32) {
 			int tokenLength = (((tokenSize - i) >= 32) ? 32 : (tokenSize - i));
 			strncpy(errorOutputString[i/32], errorString + i, tokenLength);
 			errorOutputString[i/32][tokenLength] = '\0';
-			std::cout << "tokenNumber: " << tokenNumber << "\n";
+			std::cout << "tokenNumber: " << tokenNumber << std::endl;
 			tokenNumber++;
 		}
 
@@ -157,10 +197,10 @@ void LCDDisplay::refresh()
 
 		for(int k=0;k<tokenNumber;k++) {
 			if (errorOutputString[k][0] != 0) {
-				std:: cout << " string contents: " << errorOutputString[k] <<"\n";
-				write(displayDevice, &displayOptionMode, 1);
+				std:: cout << " string contents: " << errorOutputString[k] <<std::endl;
+				write(displayDevice, displayOptionMode, 1);
 				write(displayDevice, blockNumber1, sizeof(blockNumber1));
-				write(displayDevice, errorOutputString[k], strlen(errorOutputString[k]));
+				write(displayDevice, errorOutputString[k],32);
 				sleep(5);
 			}
 		}
@@ -207,4 +247,5 @@ void LCDDisplay::writeBlock(int block, char *inputString)
 		}
 	}
 }
+
 
--- rpu/tests/display/testDisplay.cpp
+++ rpu/tests/display/testDisplay.cpp
@@ -5,29 +5,48 @@
 
 int main(int argc, char **argv)
 {
-  printf("testing display\n");
-  LCDDisplay *display = new LCDDisplay();
-  display->setPlaybackString("p");
-  display->refresh();
-  display->setPlaybackString("s");
-  //printf("track string\n");
-  display->setTrackInfoString("Now Playing: Exhibit ID 1010");
-  display->refresh();
- 
- //	display->setTrackInfoString("supercalafragalisticexpialodocious\n");	
-  //	printf("new string\n");
-  display->setMenuString("Select Language?");
-		
-  display->refresh();
-  display->setMenuString("Language Choice: Cornish");
-  //	printf("test string\n");
-  display->refresh();
-  display->setPlaybackString("R");
-  display->refresh();
-  display->displayError("ERROR: Unrecognised PIN Entered");
-  display->refresh();
+	printf("testing display\n");
+	LCDDisplay *display = new LCDDisplay();
+	display->setPlaybackString("p");
+	display->refresh();
   
-delete display;
-   printf("Done\n");
-  return 0;
+	display->setTrackInfoString("Now Playing: Exhibit ID 1010");
+	display->refresh();
+	display->setTrackInfoString("12:37 / 17:00");
+	display->refresh();
+	sleep(5);//imitates wait between listening and button press.
+	display->setPlaybackString("w");
+	display->setMenuString("Select Language?");	
+	display->refresh();
+	display->setMenuString("Language:English");
+	display->refresh();
+	display->setMenuString("Language:French");
+	display->refresh();
+	display->setMenuString("Language:German");
+	display->refresh();
+	display->setPlaybackString("r");
+	display->setTrackInfoString("Rewind");
+	display->refresh();
+	display->setMenuString("Select Info Lvl?");
+	display->refresh();
+	display->setMenuString("Ilevel:Child");
+	display->refresh();
+	display->setMenuString("Ilevel:Teenager");
+	display->refresh();
+	display->setMenuString("Ilevel:Adult");
+	display->refresh();
+	display->setPlaybackString("f");
+	display->setTrackInfoString("Fastforward");
+	display->refresh();
+	display->displayError("Unrecognised PIN Entered");
+	display->refresh();
+	display->displayError("Invalid Exhibit ID entered");
+	display->refresh();
+	display->displayError("Wireless Signal Lost");
+	display->refresh();
+	display->displayError("Thanks for using  XNOR M.A.D.S");
+	display->refresh();
+	delete display;
+	printf("Done\n");
+	return 0;
 }
