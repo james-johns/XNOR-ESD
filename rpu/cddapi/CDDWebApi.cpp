fbb7683d095a187f6f2705e4996be443c04591dd
thomasad <thomas2.deacon@live.uwe.ac.uk>
2015-05-19 18:59:00 +0100

    Added default values to knowledgeCode and languageCode

--- rpu/cddapi/CDDWebApi.cpp
+++ rpu/cddapi/CDDWebApi.cpp
@@ -220,6 +220,8 @@ int CDDWebApi::login(const char *newPin)
 	// Response is ok, filter
 	filterResponse(response);
 	strncpy (pin, newPin, 4);
+	languageCode="1";
+	knowledgeCode="1";
 	return 0;    
 	
 }
@@ -247,6 +249,8 @@ void CDDWebApi::logout()
 	curl_easy_setopt (cdd, CURLOPT_URL, toSend.c_str());
 	curl_easy_perform(cdd);
 	strncpy (pin, "    ", 4);
+	languageCode="1";
+	knowledgeCode="1";
 
 }
 
